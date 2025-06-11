4d2d59eebede75a2a77f618c06c7157f2b7c0b29
Gianpo164 <gmassoglia2023@udec.cl>
2025-06-11 03:02:01 -0400

    fix: archivo duplicado

--- frontend/src/api/paquetes.js
+++ frontend/src/api/paquetes.js
@@ -3,3 +3,6 @@ import { fetchData } from './index.js';
 // NOTE: Tengo que revisar el endpoint, ya que se supone que debe ser distinto al de la app móvil,
 // este deberia ser algo como 'paquetes-admin/listar_paquetes/' (lo compruebo en urls.py)
 export const paqueteList = (token) => fetchData('paquetes-admin/', 'GET', null, token);
+
+export const obtenerRutaPaquetes = (paquete_ids, token) =>
+  fetchData('generar_ruta/', 'POST', { paquete_ids }, token);
